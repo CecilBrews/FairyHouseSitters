b10209505767851e9a2b899f53125a32cc60e32e
Cecil <cecil.brews@gmail.com>
2021-05-13 01:29:11 -0300

    List of task part 2
    Panel with tasks remaining pops up when button is clicked, task get grayed when sprite is clicked but is back to normal when the panel is closed

--- Assets/TaskCount.cs
+++ Assets/TaskCount.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Fungus;
 
-public class TasksCompleted : MonoBehaviour
+public class TaskCount : MonoBehaviour
 {
+
+    [SerializeField] public bool TaskDone;
+
     // Start is called before the first frame update
     void Start()
     {
