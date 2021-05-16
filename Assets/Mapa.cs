27c1789c9d7228b63ddcb12c87e3cb269f1da3bb
Cecil <cecil.brews@gmail.com>
2021-05-16 01:56:07 -0300

    Juego funcionando
    Y lleno de bugs como corresponde

---
+++ Assets/Mapa.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Mapa : MonoBehaviour
+{
+    public void GoToHouse01()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
